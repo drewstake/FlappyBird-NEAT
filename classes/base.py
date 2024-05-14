4443231e40a53a4decdb9abad3953d59077f5621
drewstake <drewstake3@gmail.com>
2024-05-13 23:34:05 -0400

    Initial commit

---
+++ classes/base.py
@@ -0,0 +1,27 @@
+import pygame
+
+class Base:
+    VEL = 5
+    WIDTH = 0
+    IMG = None
+
+    def __init__(self, y, img):
+        self.y = y
+        self.IMG = img
+        self.WIDTH = img.get_width()
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, win):
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
